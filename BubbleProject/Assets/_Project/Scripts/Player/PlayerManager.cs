773dab7e9dbeca4a39ccbe73491ab36d236a2919
Joslet <a20191364@pucp.edu.pe>
2025-01-25 11:42:11 -0500

    add tests tiles and spawn points for players

---
+++ BubbleProject/Assets/_Project/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player_prefab;
+
+    [SerializeField]
+    private Transform[] spawnPoints;
+
+    private int nextPlayerIndex = 0;
+
+    public void SpawnPlayer(string controlScheme, Transform spawnPoint)
+    {
+        var player = PlayerInput.Instantiate(
+            player_prefab,                     // The prefab to instantiate
+            controlScheme: controlScheme,     // Control scheme to use (e.g., "Keyboard1", "Keyboard2")
+            pairWithDevice: Keyboard.current    // The device to pair with (optional)
+        );
+
+        player.transform.position = spawnPoint.position;
+    }
+    void Start()
+    {
+        this.SpawnPlayer("player_1", spawnPoints[0]);
+        this.SpawnPlayer("player_2", spawnPoints[1]); 
+    }
+}
