e5d00fbd48c42b0b0e38c08d742b629ec4ea464e
lmthelex <axel_ht@outlook.com>
2025-01-26 09:43:19 -0800

    collider to bullets && walls && reload

--- BubbleProject/Assets/_Project/Scripts/Player/BulletMovement.cs
+++ BubbleProject/Assets/_Project/Scripts/Player/BulletMovement.cs
@@ -6,23 +6,23 @@ public class BulletMovement : MonoBehaviour
 {
     [SerializeField]
     public float speed = 10;
-    private Quaternion direction;  // Direction the bullet will travel in
-    private float lifetime = 2f;  // Lifetime of the bullet before it is destroyed
-
-    private GunMovement gun;
-    private Rigidbody2D physics;
-
 
     void Start()
     {
-        this.gun = transform.parent.GetComponent<GunMovement>();
-        physics = GetComponent<Rigidbody2D>();
 
-        direction = this.gun.current_direction;
-        Vector2 directionVector = direction * Vector2.right; 
-        physics.velocity = directionVector * speed;  
+    }
 
-
-        Destroy(gameObject, lifetime);
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Player"))
+        {
+            Debug.Log("Shoted");
+            Destroy(gameObject);
+        }
+        if (other.collider.CompareTag("Wall"))
+        {
+            Debug.Log("Shoted");
+            Destroy(gameObject);
+        }
     }
 }
